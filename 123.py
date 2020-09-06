500d41e774d671ded896c37b2869c6aa55299653
QuangTruong23 <63149001+QuangTruong23@users.noreply.github.com>
2020-09-06 12:36:34 +0700

    Update 123.py

--- 123.py
+++ 123.py
@@ -14,25 +14,21 @@ print("-----")
 print("You choose "+ player)
 print("computer Chooses "+ computer)
 print("-----")
-
-if player == "Keo":
-	if computer == "Keo":
-		print("Draw")
-	if computer == "Dam":
-		print("Lose")
-	if computer == "La":
-		print("Win")
-if player == "Dam":
-	if computer == "Keo":
-		print("Win")
-	if computer == "Dam":
-		print("Draw")
-	if computer == "La":
-		print("Lose")		
-if player == "La":
-	if computer == "Keo":
-		print("Lose")
-	if computer == "Dam":
-		print("Win")
-	if computer == "La":
-		print("Draw")
+if plyer == compuer :
+	print("Draw")
+else:
+	if player == "Keo":
+		if computer == "Dam":
+			print("Lose")
+		if computer == "La":
+			print("Win")
+	if player == "Dam":
+		if computer == "Keo":
+			print("Win")	
+		if computer == "La":
+			print("Lose")		
+	if player == "La":
+		if computer == "Keo":
+			print("Lose")
+		if computer == "Dam":
+			print("Win")
