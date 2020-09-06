661f6bc02fd50d467a9232feac3a585ac62d6e76
QuangTruong23 <63149001+QuangTruong23@users.noreply.github.com>
2020-09-06 13:15:51 +0700

    hoan thien chuong trinh Dam La Keo

--- 123.py
+++ 123.py
@@ -3,32 +3,41 @@ print ("Nhap Dam, La, Keo: ")
 
 player = str (input())
 computer = randint(0,2)
-
+	
 if computer == 0:
 	computer = "Dam"
 if computer == 1:
 	computer = "La"
 if computer == 2:
 	computer = "Keo"
-print("-----")
-print("You choose "+ player)
-print("computer Chooses "+ computer)
-print("-----")
-if plyer == compuer :
+	print("-----")
+	print("You choose: "+ player)
+	print("Computer Chooses:"+ computer)
+	print("-----")	
+
+if player == computer :
 	print("Draw")
-else:
+	
+else:		
 	if player == "Keo":
 		if computer == "Dam":
 			print("Lose")
-		if computer == "La":
+		else:
 			print("Win")
-	if player == "Dam":
+
+	elif player == "Dam":
 		if computer == "Keo":
 			print("Win")	
-		if computer == "La":
-			print("Lose")		
-	if player == "La":
+		else:
+			print("Lose")
+
+	elif player == "La":
 		if computer == "Keo":
 			print("Lose")
-		if computer == "Dam":
+		else:	
 			print("Win")
+	else :
+		print("Nhap sai! Can nhap lai ")
+
+			
+
